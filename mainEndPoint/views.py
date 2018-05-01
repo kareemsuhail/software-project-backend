088eb8e84200685e32b2f76593b9e7dfe6674f79
kareem ayesh <1kayesh@students.iugaza.edu.ps>
2018-05-01 14:37:24 +0300

    adding import for require http method

--- mainEndPoint/views.py
+++ mainEndPoint/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from django.shortcuts import HttpResponse
+from django.views.decorators.http import require_http_methods
 import jwt
 @csrf_exempt
 @require_http_methods([ "POST"])
